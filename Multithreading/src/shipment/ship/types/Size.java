26c160758b12cc0743b4f774fbcca8ff301eeccf
Vadim <shkrtdamn@gmail.com>
2021-06-13 23:31:37 +0300

    changes

--- Multithreading/src/shipment/ship/types/Size.java
+++ Multithreading/src/shipment/ship/types/Size.java
@@ -1,7 +1,7 @@
 package shipment.ship.types;
 
 public enum Size {
-    SMALL(10), MIDDLE(50), LARGE(100);
+    SMALL(10), MIDDLE(50), LARGE(100), SUPER_LARGE(1000);
     private int value;
 
     Size(int value) {
